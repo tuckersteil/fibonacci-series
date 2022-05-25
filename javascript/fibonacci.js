d9443f07a4f52cc8502c90f33c6d9ac41c14a322
Tucker Steil <tuckersteil@gmail.com>
2022-05-25 14:52:31 -0400

    commit tree 6131ab07a6dbc34cdc5f74683d3e48e372710fc8

--- javascript/fibonacci.js
+++ javascript/fibonacci.js
@@ -1,12 +1,39 @@
 function fibonacci(num) {
-  // type your code here
+  if(num === 0){
+    return num
+  }
+  else if (num === 1){
+    return num
+  }
+  else{
+      let array = []
+  for (let i = 0; i < num; i++){
+    array.push(i - 0)
+  }
+  let newA = []
+  for(let i = 0; i < array.length; i++){
+    if(array[i] === array[0]){
+      array[i] = 0
+      newA.push(array[i])
+    } 
+    else if(array[i] === array[1]){
+      array[i] = 1
+      newA.push(array[i])
+    }
+    else{
+      array[i] = array[i-1] + array[i-2]
+      newA.push(array[i])
+    }
+   }
+    return (newA[newA.length - 1] + newA[newA.length - 2])
+  }  
 }
 
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: 0");
   console.log("=>", fibonacci(0));
-
+  
   console.log("");
 
   console.log("Expecting: 1");
